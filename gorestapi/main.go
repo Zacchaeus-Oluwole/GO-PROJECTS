6e54210e1315b71e4f9c36fb2f14d8c42aa2443f
Zacchaeus-Oluwole <zacchaeusoluwole@gmail.com>
2023-02-05 22:19:23 +0100

    GO Rest Api Using Gorilla Mux

--- gorestapi/main.go
+++ gorestapi/main.go
@@ -22,11 +22,32 @@ func getBooks(w http.ResponseWriter, r *http.Request){
 	json.NewEncoder(w).Encode(books)
 }
 
+//Get single Bool
+func getBook(w http.ResponseWriter, r *http.Request){
+	fmt.Println("Status-Code 200 Successful...")
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r) //Get params
+	//Loop through books and find with id
+	for _, item := range books {
+		if item.ID == params["id"] {	
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&models.Book{})
+	
+}
+
+
+
+
 func main()  {
 	//Init Router
 	r := mux.NewRouter()
 
 	//Mock Data - @todo - implement DB
+	//Init books var as a slice Book struct
+	// var books []models.Book
 
 	books = append(books, models.Book{ID: "1", Isbn: "12345", 
 	Title: "First Book", Author: &models.Author{Firstname: "Zacchaeus", Lastname: "Oluwole"}})
@@ -36,10 +57,12 @@ func main()  {
 
 	// Route Handlers / Endpoints
 	r.HandleFunc("/api/books", getBooks).Methods("GET")
-	// r.HandleFunc("api/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("api/books/{id}", getBook).Methods("GET")
 	// r.HandleFunc("api/books", createBook).Methods("POST")
 	// r.HandleFunc("api/books/{id}", updateBook).Methods("PUT")
 	// r.HandleFunc("api/books/{id}", deleteBook).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":8000", r))
+
+
 }
