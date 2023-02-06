bb1958de813bfc1257f5a27536abb04350a36a25
Zacchaeus-Oluwole <zacchaeusoluwole@gmail.com>
2023-02-06 13:41:03 +0100

    GO Rest Api Using Gorilla Mux

--- gorestapi/main.go
+++ gorestapi/main.go
@@ -11,33 +11,43 @@ import (
 	"gorestapi/src"
 )
 
+
 //Init books var as a slice Book struct
 var books []models.Book
 
 //Get all books
 func getBooks(w http.ResponseWriter, r *http.Request){
-	fmt.Println("Status-Code 200 Successful...")
-
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(books)
 }
 
-//Get single Bool
-func getBook(w http.ResponseWriter, r *http.Request){
-	fmt.Println("Status-Code 200 Successful...")
+// Get single book
+func getBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	params := mux.Vars(r) //Get params
-	//Loop through books and find with id
+	params := mux.Vars(r) // Gets params
+	// Loop through books and find one with the id from the params
 	for _, item := range books {
-		if item.ID == params["id"] {	
+		if item.ID == params["id"] {
 			json.NewEncoder(w).Encode(item)
 			return
 		}
 	}
 	json.NewEncoder(w).Encode(&models.Book{})
-	
 }
 
+// //Create a New Book
+// func createBook(w http.ResponseWriter, r *http.Request){
+// 	w.Header().Set("Content-Type", "application/json")
+// 	var book Book
+// 	_ = json.NewDecoder(r.Body).Decode(&book)
+// 	for _, item := range books {
+// 		if item.ID == params["id"] {	
+// 			json.NewEncoder(w).Encode(item)
+// 			return
+// 		}
+// 	}
+// 	book.ID = strconv.Itoa(books.len() + 1)
+// }
 
 
 
@@ -57,10 +67,11 @@ func main()  {
 
 	// Route Handlers / Endpoints
 	r.HandleFunc("/api/books", getBooks).Methods("GET")
-	r.HandleFunc("api/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
 	// r.HandleFunc("api/books", createBook).Methods("POST")
 	// r.HandleFunc("api/books/{id}", updateBook).Methods("PUT")
 	// r.HandleFunc("api/books/{id}", deleteBook).Methods("DELETE")
+	fmt.Println(len(books)+1)
 
 	log.Fatal(http.ListenAndServe(":8000", r))
 
